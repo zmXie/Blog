338eb353adf7a93522f07f04293f6619c392dbf1
xiezhimin <xiezm@dachentech.com.cn>
2022-02-10 10:27:46 +0800

    feat: add doc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
       "link",
       {
         "rel": "icon",
-        "href": "/avatar.png"
+        "href": "/favicon.ico"
       }
     ],
     [
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 npm run build
 
+echo '构建完成'  
+
 cd public
 
 git init
 git add -A
 git commit -m 'deploy'
 
+echo '正在推送中..'  
+
 git push -f https://github.com/zmXie/zmXie.github.io.git master
+
+echo '推送完成'  
 
 cd ../
 rm -rf public
+
+echo '本地构建产物已删除'  
