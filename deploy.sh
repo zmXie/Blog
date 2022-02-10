2f578908a742a0b4112560a9a813720bb0f2eab1
xiezhimin <xiezm@dachentech.com.cn>
2022-02-10 10:09:27 +0800

    first

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git remote add origin https://gitee.com/xzm-ddpz/gitee.io.git
-git push -u origin "master"
+git push -f https://github.com/zmXie/zmXie.github.io.git master
 
 cd ../
 rm -rf public
