484608b4e51b725d85d9eddebb066a211a80b2ff
xiezhimin <xiezm@dachentech.com.cn>
2022-03-01 20:12:25 +0800

    feat: add doc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -69,6 +69,11 @@ module.exports = {
         link: "https://valine.js.org/"
       },
       {
+        title: "ProcessOn",
+        desc: "在线做图工具",
+        link: "https://www.processon.com/diagrams"
+      },
+      {
         title: "午后南杂",
         desc: "Enjoy when you can, and endure when you must.",
         link: "https://www.recoluan.com"
