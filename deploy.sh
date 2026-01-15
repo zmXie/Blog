ca4170174ca1d44f4c01c2e3c374256f3411c659
谢志敏 <xiezm@dachentech.com.cn>
2026-01-15 10:18:13 +0800

    feat: add doc

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if npm run build; then
     echo '构建完成'
-    cd public
+    cd dist
     git init
     git add -A
     git commit -m 'deploy'
@@ -8,7 +8,7 @@ if npm run build; then
     if git push -f git@github.com:zmXie/zmXie.github.io.git main; then
         echo '推送完成'
         cd ../
-        rm -rf public
+        rm -rf dist
         echo '本地构建产物已删除'
         npm run push
     fi
